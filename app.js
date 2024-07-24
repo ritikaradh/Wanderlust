b43eefb77c20e4a6b0d1b29026363a80fd2ebe5e
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-07-24 17:43:38 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+main()
+    .then(()=>{
+        console.log("Database Connected!");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.listen(8080, ()=>{
+    console.log("server is listening to port 8080");
+});
+
+app.get("/", (req,res)=>{
+    res.send("Hi, i am root");
+});
