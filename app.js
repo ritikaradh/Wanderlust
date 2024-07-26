1228f37d2ca2e82d237521409c2112f875db46d1
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-07-26 21:05:43 +0530

    handling routers

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 main()
     .then(()=>{
@@ -45,4 +49,50 @@ app.get("/", (req,res)=>{
 app.get("/listings", async(req,res)=>{
     const allListings = await Listing.find({});
     res.render("Listings/index.ejs", {allListings});
+});
+
+//new route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//new route
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//Create Route
+app.post("/listings", async(req,res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//Update Route
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 });
