83344583a14f692aca15f8be1b5cf960487a1bf6
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-09-14 23:31:33 +0530

    Authentication: Implementing Signup and Login for users

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const sessionOptions = {
     secret: "mysupersecretcode",
@@ -26,15 +29,32 @@ app.get("/", (req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+app.get("/demouser", async(req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    });
 
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+});
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -59,10 +79,9 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-
-
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next)=>{
     next(new ExpressError(404, "Page Not Found"));
