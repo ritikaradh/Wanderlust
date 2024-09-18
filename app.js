4e6c6702c45cb49df56c7ca26cba51d8896fdedf
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-09-19 00:21:11 +0530

    Styling rating section using starability in show listings and implementing upload image feature in add new listing

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_env != "production"){
+    require("dotenv").config();
+}
+
 const express = require("express");
 const engine = require("ejs-mate");
 const app = express();
--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,16 @@ const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const ListingControllers = require("../controllers/listing.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router.route("/")
     .get(wrapAsync(ListingControllers.index)) //index route
-    .post(isLoggedIn, validateListing, wrapAsync(ListingControllers.createListings)); //create route
+    // .post(isLoggedIn, validateListing, wrapAsync(ListingControllers.createListings)); //create route
+    .post(upload.single('listing[image]'), (req,res) => {
+        res.send(req.file);
+    });
 
 router.get("/new", isLoggedIn, ListingControllers.renderNewForms); //new route
 
